70dc6e77f34676c8b9e6b9deace5cc923545298b
Nguyen-Ngoc-Linht <104997223+Nguyen-Ngoc-Linht@users.noreply.github.com>
2024-09-13 16:38:17 +0700

    uopup

--- be-clea-webapp/src/index.js
+++ be-clea-webapp/src/index.js
@@ -6,7 +6,34 @@ const morgan = require("morgan");
 const app = express();
 const db = require("./config/db");
 
+const http = require("http");
+const socketIo = require("socket.io");
+
 const route = require("./routes");
+
+// Socket
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Cho phép tất cả các nguồn truy cập, nếu cần có thể cấu hình lại
+    methods: ["GET", "POST"],
+  },
+});
+
+// Xử lý sự kiện kết nối socket
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  // Lắng nghe và phát dữ liệu từ client
+  socket.on("new message", (data) => {
+    console.log("Received message: ", data);
+    io.emit("message", data); // Phát lại tin nhắn tới tất cả các client
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 // Cookie
 app.use(cookieParser());
@@ -37,6 +64,10 @@ app.use(express.json());
 //Router init
 route(app);
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
+
+server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
